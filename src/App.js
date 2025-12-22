bae4ed02f05c2397121f87c211e3f4de52057b28
umejrr <umejrtahirovic@gmail.com>
2025-12-22 05:16:30 +0100

    reworked topbar a bit and started working on the css of the addcards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import head from './imgs/Frame 23.png'
 import mouthBorder from './imgs/Vector 7.png'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import body from './imgs/body.png'
 
 
 const initialDays = [
@@ -98,10 +99,15 @@ function CardTable() {
     <header className="App-header">
         <div className="padding-global">
         <div className="container">
-
+          <a className='logout'>log out</a>
+        <div className="welcome-wrap">
+          <h1>
+            Hello, Umi...
+          </h1>
+        </div>
         <div className="topbar">
             <div className="topbar-heading-wrap">
-              <h1>LAST 10 DAYz</h1>
+              <h2>LAST 10 DAYz</h2>
             </div>
             <div className="topbar-btns-wrap">
               <a href="#" className="checklist">
@@ -131,57 +137,44 @@ function CardTable() {
                 </ul>
                 
             </div>
-            
+            <div className='body'><img src={body}/></div>
           </div>
         </div>
 
-        
-        <a className="add-btn" onClick={()=> setModalOpen(true)}>
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7H13" stroke="white" stroke-width="2" stroke-linecap="round"/>
-            <path d="M7 1L7 13" stroke="white" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-        </a>
-
         <div className="card-table-wrapper">
-
-
-                
-            
-
         </div>
-
-        
           <dialog open={modalOpen}>
               <div className="dialog-wrap">
                   <h2>Add new card</h2>
-                  <form onSubmit={handleSubmit}>
-                      <div className="fields-wrap">
-                          <div>
-                              <label>Date:</label>
-                              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}></DatePicker>
-                          </div>
-                          <div>
-                              <label>Morning:</label>
-                              <select value={morningValue} onChange={handleChangeMorning}>
-                                 <option value={''}>Pick one</option>
-                                 <option>Bronze</option>
-                                 <option>Silver</option>
-                                 <option>Gold</option>
-                              </select>
-                          </div>
-                          <div>
-                              <label>Night:</label>
-                              <select value={nightValue} onChange={handleChangeNight}>
-                                 <option value={''}>Pick one</option>
-                                 <option>Bronze</option>
-                                 <option>Silver</option>
-                                 <option>Gold</option>
-                              </select>
-                          </div>
-                      </div>
-                      <input type='submit' disabled={!morningValue || !nightValue}  value={'Add'}></input>
-                  </form>
+                  <div class="form-wrap">
+                    <form onSubmit={handleSubmit}>
+                        <div className="fields-wrap">
+                            <div className='input-wrap date'>
+                                <label>Date:</label>
+                                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}></DatePicker>
+                            </div>
+                            <div className='input-wrap morning'>
+                                <label>Morning:</label>
+                                <select value={morningValue} onChange={handleChangeMorning}>
+                                   <option value={''}>Pick one</option>
+                                   <option>Bronze</option>
+                                   <option>Silver</option>
+                                   <option>Gold</option>
+                                </select>
+                            </div>
+                            <div className='input-wrap night'>
+                                <label>Night:</label>
+                                <select value={nightValue} onChange={handleChangeNight}>
+                                   <option value={''}>Pick one</option>
+                                   <option>Bronze</option>
+                                   <option>Silver</option>
+                                   <option>Gold</option>
+                                </select>
+                            </div>
+                        </div>
+                        <input type='submit' disabled={!morningValue || !nightValue}  value={'Add'}></input>
+                    </form>
+                  </div>
                   <a onClick={()=> setModalOpen(false)} className='dialog-close'><svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1 1.00024C1.11851 1.35758 1.71467 2.19256 2.6125 3.15144C3.68599 4.29793 5.36345 5.3637 6.35376 6.02629C7.3278 6.67801 8.35861 7.70164 9.8526 9.10495C10.2153 9.43804 10.4523 9.61582 10.7226 9.85554C10.9928 10.0953 11.2891 10.3915 11.774 10.8764" stroke="black" stroke-width="2" stroke-linecap="round"/>
                   <path d="M10.5156 1.00024C10.3971 1.35758 9.91949 2.07405 9.29101 2.79321C8.71087 3.45706 8.06637 4.22884 7.13711 5.1563C5.75394 6.5368 4.35295 8.35885 3.8735 9.07802C3.33481 9.73792 2.79611 10.0971 2.43788 10.3655C2.25741 10.5154 2.07964 10.6932 1.89648 11.0559" stroke="black" stroke-width="2" stroke-linecap="round"/>
